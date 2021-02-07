d2aeab010d446e0be0b59dca74dd826da73176f8
MyronovychOleksandr <muronalex@ukr.net>
2021-02-07 17:25:45 +0200

    added toggle filter

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,29 +1,44 @@
-import { React } from "react";
+import { React, Component } from "react";
+import { CSSTransition } from "react-transition-group";
+import { connect } from "react-redux";
+
 import PhonebookForm from "./phonebookForm/PhonebookForm";
 import Filter from "./filter/Filter";
 import PhonebookList from "./phonebookList/PhonebookList";
-import { CSSTransition } from "react-transition-group";
 import s from "./App.module.css";
 import "./animation/fadeApp.css";
 
-const App = () => (
-  <div className={s.container}>
-    <CSSTransition
-      in={true}
-      appear={true}
-      timeout={500}
-      classNames="header"
-      unmountOnExit
-    >
-      <h2 className={s.phonebookHeader}>Phonebook</h2>
-    </CSSTransition>
-    <PhonebookForm />
-    <CSSTransition in={2 > 1} timeout={250} classNames="filter" unmountOnExit>
-      <Filter />
-    </CSSTransition>
+class App extends Component {
+  render() {
+    return (
+      <div className={s.container}>
+        <CSSTransition
+          in={true}
+          appear={true}
+          timeout={500}
+          classNames="header"
+          unmountOnExit
+        >
+          <h2 className={s.phonebookHeader}>Phonebook</h2>
+        </CSSTransition>
+        <PhonebookForm />
+        <CSSTransition
+          in={this.props.contacts.length > 1}
+          timeout={250}
+          classNames="filter"
+          unmountOnExit
+        >
+          <Filter />
+        </CSSTransition>
 
-    <PhonebookList />
-  </div>
-);
+        <PhonebookList />
+      </div>
+    );
+  }
+}
 
-export default App;
+const mapStateToProps = (state) => ({
+  contacts: state.phonebook.contacts,
+});
+
+export default connect(mapStateToProps)(App);
